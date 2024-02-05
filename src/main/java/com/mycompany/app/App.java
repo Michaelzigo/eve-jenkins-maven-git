1b6bbd3ebd0037893823f2990e9c510a228d8827
root <root@ip-172-31-27-244.eu-west-2.compute.internal>
2024-02-05 13:06:40 +0000

    Second Commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World How Are We Doing!" );
+        System.out.println( "Hello World How Are We Doing Lets Get IT!" );
     }
 }
